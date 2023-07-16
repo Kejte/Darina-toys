a5621ecdeb72cff5839988958766324ee8f1f200
Kejte <kejte1@mail.ru>
2023-07-16 17:25:53 +0300

    refactor envvars

--- darinatoys/settings.py
+++ darinatoys/settings.py
@@ -9,10 +9,11 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import dotenv
 from pathlib import Path
 from datetime import timedelta
 import os
+dotenv.load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY='Vika_dhfgudfhgudfg@#'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -82,9 +83,9 @@ WSGI_APPLICATION = 'darinatoys.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'darianatoys',
-        'USER': 'postgres',
-        'PASSWORD': '0987654321weew',
+        'NAME': os.getenv('NAME_DB'),
+        'USER': os.getenv('USER_DB'),
+        'PASSWORD': os.getenv('PASSWORD_DB'),
         'HOST': 'pgdb',
         'PORT': '5432'
     }
