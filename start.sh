b86a4b88f00e700250c1e73e323231e31a6c0022
Kejte <kejte1@mail.ru>
2023-08-11 15:29:16 +0300

    add certbot

--- start.sh
+++ start.sh
@@ -6,4 +6,5 @@ set -o nounset
 python manage.py collectstatic --no-input
 python manage.py makemigrations --no-input && python manage.py migrate
 python manage.py migrate
+
 gunicorn darinatoys.wsgi:application --bind 0.0.0.0:8000
